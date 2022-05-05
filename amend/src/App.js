06ad9523b1a6a07e2ceacd04c0393ae8ba10282f
Maheash <maheash.mmk17@gmail.com>
2022-05-05 22:35:18 +0530

    Changes made

--- amend/src/App.js
+++ amend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Practice from './Practice';
-import Navbar from './navbar';
+// import Appbar from './Bars/Appbar';
+// import Signup from './Login//Signup';
+import Dash from './Login/Signin';
 function App() {
   return (
-    <>
-    <Practice></Practice>
-    {/* <Navbar></Navbar> */}
-    </>
+      // <Signup></Signup>
+      <Dash></Dash>
       );
 }
 

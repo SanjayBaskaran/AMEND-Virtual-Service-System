cb2368761ebb2673c54708a03f2382b8ab5e3062
nandini-212 <nandyrp23@gmail.com>
2022-05-28 16:36:43 +0530

    Python Flask env update

--- amend_flask_mailing_system/env/Scripts/activate.bat
+++ amend_flask_mailing_system/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Codings\Python\env
+set VIRTUAL_ENV=E:\AMEND-Virtual-Service-System\amend_flask_mailing_system\env
 
 if not defined PROMPT set PROMPT=$P$G
 
